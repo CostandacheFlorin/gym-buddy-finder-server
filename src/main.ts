7d494d1b36d67caf848b0b58a78af84aaa7b8e78
Costandache Florin <costandacheflorinadrian@gmail.com>
2024-09-29 11:20:41 +0300

    changed how the cookie parser is being improted

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
